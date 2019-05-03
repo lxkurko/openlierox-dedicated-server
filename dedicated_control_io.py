c527d7cdce11c99be1de79667c807861f51a67ca
lxkurko <lxkurko@users.noreply.github.com>
2019-05-03 22:40:01 +0300

    io: picked updates from upstream

--- dedicated_control_io.py
+++ dedicated_control_io.py
@@ -45,6 +45,9 @@ def getSignal():
 def setvar(what, data):
 	SendCommand( "setvar %s \"%s\"" % (str(what), str(data)) )
 
+def setWormWeapons(wormid, weapon1, weapon2, weapon3, weapon4, weapon5):
+	SendCommand( "setWormWeapons %d \"%s\" \"%s\" \"%s\" \"%s\" \"%s\"" % (wormid, weapon1, weapon2, weapon3, weapon4, weapon5) )
+
 # Use this to make the server quit
 def Quit():
 	SendCommand( "quit" )
@@ -81,6 +84,9 @@ def kickBot(msg = None):
 		SendCommand( "kickbot \"%s\"" % str(msg) )
 	else:
 		SendCommand( "kickbot" )
+
+def kickBots():
+	SendCommand( "kickbots" )
 
 # Suicides all local bots
 def killBots():
@@ -143,6 +149,15 @@ def getWormPing(iID):
 def getWormSkin(iID):
 	ret = SendCommand( "getwormskin %i" % int(iID) )
 	return ( int(ret[0]), ret[1].lower() )
+
+def getWormVersion(iID):
+	ret = SendCommand( "whoIs %i" % int(iID) )
+	if len(ret) == 0:
+		return "0.57_Beta1"
+	for i in ret:
+		if i.startswith("Version: "):
+			return i.replace("Version: ", "", 1)
+	return "0.57_Beta1"
 
 def getVar(var):
 	ret = SendCommand( "getvar %s" % var )
